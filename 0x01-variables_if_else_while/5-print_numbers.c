aee7ae5b687b47653d75e9fb0e9acd4672461a7c
Ibrahim Ganiu <ibrahimganiu22@gmail.com>
2022-12-12 15:33:28 -0800

    Numberz

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,49 +1,26 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 
-
-
 /**
+ * main - return single integer from 0-10
  *
- *  * main - return single integer from 0-10
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
-
-
 {
+	int num = '0';
 
-		int num = '0';
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
 
+	}
+	
+	putchar('\n');
 
-
-			while (num <= '9')
-
-					{
-
-								putchar(num);
-
-										num++;
-
-											}
-
-
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	return (0);
 
 
 }
