e9118b59858f1fa49b7773ce01ebcc1e403a7bc0
Ibrahim Ganiu <ibrahimganiu22@gmail.com>
2022-12-16 10:34:37 -0800

    3print

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H 
 
+#include <stdio.h>
+
+
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -11,5 +15,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+int _putchar(char c);
 
 #endif 
