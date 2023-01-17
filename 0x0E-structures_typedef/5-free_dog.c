b889fbe1dc7dff5647eacb5a025273189cb9a7da
Ibrahim Ganiu <ibrahimganiu22@gmail.com>
2023-01-17 12:31:07 -0800

    update

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,32 +4,28 @@
 
 
 
+/**
+ *
+ *  * free_dog - frees memory allocated for a struct dog
+ *
+ *   * @d: struct dog to free
+ *
+ *    */
 
+void free_dog(dog_t *d)
 
-	/**
-	 *
-	 * 	 * free_dog - frees memory allocated for a struct dog
-	 *
-	 * 	 	 * @d: struct dog to free
-	 *
-	 */
+{
 
-	void free_dog (dog_t *d)
+		if (d)
 
-		{
+				{
 
-					if (d)
+							free(d->name);
 
-								{
+									free(d->owner);
 
-												free(d->name);
+											free(d);
 
-															free(d->owner);
+												}
 
-																		free(d);
-
-																				}
-
-						}
-
-
+}
